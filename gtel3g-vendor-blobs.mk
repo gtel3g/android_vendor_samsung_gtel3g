7cee19689ffbdbd66678d7d43dd675d6046282eb
Ma3a_XaTa <v.radulevich@gmail.com>
2026-08-29 22:19:02 +0300

    gtel3g: absorb legacy scx30g proprietary blobs

--- gtel3g-vendor-blobs.mk
+++ gtel3g-vendor-blobs.mk
@@ -32,3 +32,30 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/gtel3g/proprietary/lib/libreference-ril_sp.so:system/vendor/lib/libreference-ril_sp.so \
     vendor/samsung/gtel3g/proprietary/lib/libuvdenoise.so:system/vendor/lib/libuvdenoise.so \
     vendor/samsung/gtel3g/proprietary/vendor/firmware/BCM43430A1_001.002.009.0022.0000_Samsung_TabE_SM-T561_TEST_ONLY.hcd:system/vendor/firmware/BCM43430A1_001.002.009.0022.0000_Samsung_TabE_SM-T561_TEST_ONLY.hcd
+
+# Legacy SCX30G proprietary blobs
+PRODUCT_COPY_FILES += \
+    vendor/samsung/gtel3g/proprietary/bin/gpsd:system/vendor/bin/gpsd \
+    vendor/samsung/gtel3g/proprietary/bin/gps.cer:system/vendor/bin/gps.cer \
+    vendor/samsung/gtel3g/proprietary/lib/libsprd_agps_agent.so:system/vendor/lib/libsprd_agps_agent.so \
+    vendor/samsung/gtel3g/proprietary/lib/hw/gps.default.so:system/vendor/lib/hw/gps.default.so \
+    vendor/samsung/gtel3g/proprietary/lib/libfactoryutil.so:system/vendor/lib/libfactoryutil.so \
+    vendor/samsung/gtel3g/proprietary/lib/libatparser.so:system/vendor/lib/libatparser.so \
+    vendor/samsung/gtel3g/proprietary/vendor/firmware/vbc_eq:system/vendor/firmware/vbc_eq \
+    vendor/samsung/gtel3g/proprietary/bin/macloader:system/vendor/bin/macloader \
+    vendor/samsung/gtel3g/proprietary/bin/nvitemd:system/vendor/bin/nvitemd \
+    vendor/samsung/gtel3g/proprietary/bin/refnotify:system/vendor/bin/refnotify \
+    vendor/samsung/gtel3g/proprietary/bin/at_distributor:system/vendor/bin/at_distributor \
+    vendor/samsung/gtel3g/proprietary/bin/engpc:system/vendor/bin/engpc \
+    vendor/samsung/gtel3g/proprietary/bin/ext_data.sh:system/vendor/bin/ext_data.sh \
+    vendor/samsung/gtel3g/proprietary/bin/ext_kill.sh:system/vendor/bin/ext_kill.sh \
+    vendor/samsung/gtel3g/proprietary/bin/ext_symlink.sh:system/vendor/bin/ext_symlink.sh \
+    vendor/samsung/gtel3g/proprietary/lib/libsecnativefeature.so:system/vendor/lib/libsecnativefeature.so \
+    vendor/samsung/gtel3g/proprietary/lib/libomission_avoidance.so:system/vendor/lib/libomission_avoidance.so \
+    vendor/samsung/gtel3g/proprietary/lib/libomx_aacdec_sprd.so:system/vendor/lib/libomx_aacdec_sprd.so \
+    vendor/samsung/gtel3g/proprietary/lib/libomx_mp3dec_sprd.so:system/vendor/lib/libomx_mp3dec_sprd.so \
+    vendor/samsung/gtel3g/proprietary/lib/libomx_avcdec_hw_sprd.so:system/vendor/lib/libomx_avcdec_hw_sprd.so \
+    vendor/samsung/gtel3g/proprietary/lib/libomx_avcenc_hw_sprd.so:system/vendor/lib/libomx_avcenc_hw_sprd.so \
+    vendor/samsung/gtel3g/proprietary/lib/libomx_vpxdec_hw_sprd.so:system/vendor/lib/libomx_vpxdec_hw_sprd.so \
+    vendor/samsung/gtel3g/proprietary/lib/libomx_m4vh263dec_hw_sprd.so:system/vendor/lib/libomx_m4vh263dec_hw_sprd.so \
+    vendor/samsung/gtel3g/proprietary/lib/libomx_m4vh263enc_hw_sprd.so:system/vendor/lib/libomx_m4vh263enc_hw_sprd.so
